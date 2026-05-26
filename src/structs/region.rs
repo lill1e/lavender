4de6d401535abe53e0a40caee4c7c04f61880e39
lillie <onlinechronically@protonmail.com>
2026-05-26 13:34:23 -0400

    add: Region+Shard for Riot Server endpoints

--- src/structs/region.rs
+++ src/structs/region.rs
@@ -1,11 +1,65 @@
+use std::fmt::Display;
+
 use anyhow::Result;
 use serde::Deserialize;
 
 use crate::structs::{callable::Callable, lockfile::LockFile};
 
+#[derive(Debug)]
+pub enum Region {
+    Na,
+    Latam,
+    Br,
+    Eu,
+    Ap,
+    Kr,
+}
+
+#[derive(Debug)]
+pub enum Shard {
+    Na,
+    Pbe,
+    Eu,
+    Ap,
+    Kr,
+}
+
+impl Display for Region {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{}",
+            match self {
+                Region::Na => "na",
+                Region::Latam => "latam",
+                Region::Br => "br",
+                Region::Eu => "eu",
+                Region::Ap => "ap",
+                Region::Kr => "kr",
+            }
+        )
+    }
+}
+
+impl Display for Shard {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{}",
+            match self {
+                Shard::Na => "na",
+                Shard::Pbe => "pbe",
+                Shard::Eu => "eu",
+                Shard::Ap => "ap",
+                Shard::Kr => "kr",
+            }
+        )
+    }
+}
+
 #[derive(Debug, Deserialize)]
 #[serde(rename_all = "camelCase")]
-pub struct Region {
+pub struct RegionData {
     locale: String,
     region: String,
     web_language: String,
@@ -13,7 +67,7 @@ pub struct Region {
 }
 
 impl LockFile {
-    pub fn region(&self) -> Result<Region> {
+    pub fn region(&self) -> Result<RegionData> {
         self.get("/riotclient/region-locale")
     }
 }
